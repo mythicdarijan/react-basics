3d5bad1a4922eb395e413a6d5b735dcdc9ccb79a
Darijan <darijang12@gmail.com>
2025-03-11 14:39:52 +0100

    reverted name to food.jsx due to errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
-import Food from './Food.jsx'
+import Food from './food.jsx'
 
 function App() {
   return(
     <>
         <Header/>
         <Footer/>
+        <Food/>
     </>
   );
 }
