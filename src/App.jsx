a4b4f82b4c57b1ff0657fe6d302dce739e72b3df
Darijan <darijang12@gmail.com>
2025-04-08 12:50:45 +0200

    imported usergreetings component into app component, creating a function that declares what content shows up depending if the user is logged inor not

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Student from './Student.jsx'
+import UserGreeting from "./UserGreetings.jsx";
 
 function App() {
   return(
     <>
-    <Student name="Sponge Bob" age="30" isStudent={true}/>
-    <Student name="Patrick" age={42} isStudent={false}/>
-    <Student name="Squidward" age={50} isStudent={false}/>
-    <Student name="Sandy" age={27} isStudent={true}/>
-    <Student/>
+      <UserGreeting isLoggedIn={false} username="Darijan"/>
     </>
   );
 }
