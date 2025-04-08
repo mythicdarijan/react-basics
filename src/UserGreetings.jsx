adde680f6620f654cba202cfcc5d4c07e45519a1
Darijan <darijang12@gmail.com>
2025-04-08 13:13:01 +0200

    used another different approach to the conditional by using constants, added prop types too

--- src/UserGreetings.jsx
+++ src/UserGreetings.jsx
@@ -1,5 +1,16 @@
+import PropTypes from 'prop-types';
+
 function UserGreeting(props){
-return(props.isLoggedIn ?   <h2 className="welcome-message">Welcome {props.username}</h2> : 
-                            <h2 className="login-prompt">Please log in to continue</h2>);
+
+    const welcomeMessage = <h2 className="welcome-message">Welcome {props.username}</h2>
+    const loginPrompt = <h2 className="login-prompt">Please log in to continue</h2>
+
+    return(props.isLoggedIn ? welcomeMessage : loginPrompt);
 }
+
+UserGreeting.PropTypes = {
+    isLoggedIn: PropTypes.bool,
+    username: PropTypes.string,
+}
+
 export default UserGreeting
